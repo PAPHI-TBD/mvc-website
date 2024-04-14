7010640a200402e8e3371b9408c38d994842b531
devenshah2018 <46404643+devenshah2018@users.noreply.github.com>
2024-04-14 14:13:39 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mingley_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
